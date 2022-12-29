2117fd225c1fb558490bdd73961da4ca7caf70b9
Szymon <szynon.oz@gmail.com>
2022-12-29 22:57:34 +0100

    Wyświetlanie statystyk

--- src/main/java/agh/gui/GuiStatisticsModule.java
+++ src/main/java/agh/gui/GuiStatisticsModule.java
@@ -2,6 +2,7 @@ package agh.gui;
 
 
 import agh.oop.AbstractWorldMap;
+import agh.simulation.SimulationParameters;
 import agh.statistics.StatisticsModule;
 import javafx.application.Platform;
 import javafx.geometry.HPos;
@@ -9,6 +10,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.Locale;
 
 public class GuiStatisticsModule extends VBox {
 
@@ -20,7 +23,7 @@ public class GuiStatisticsModule extends VBox {
         this.statisticsModule = statisticsModule;
         this.stage = stage;
         this.statisticGrid.setGridLinesVisible(true);
-        statisticGrid.setBackground(new Background(new BackgroundFill(Color.rgb(115, 180, 110), null, null)));
+        statisticGrid.setBackground(new Background(new BackgroundFill(Color.rgb(125, 210, 235), null, null)));
         getChildren().addAll(statisticGrid);
         createGrid();
     }
@@ -31,15 +34,17 @@ public class GuiStatisticsModule extends VBox {
         statisticGrid.getRowConstraints().clear();
         statisticGrid.getColumnConstraints().clear();
 
-        Label xyLabel = new Label("y\\x");
-        GridPane.setHalignment(xyLabel, HPos.CENTER);
-        this.statisticGrid.getColumnConstraints().add(new ColumnConstraints(200));
-        this.statisticGrid.getRowConstraints().add(new RowConstraints(50));
-        this.statisticGrid.add(xyLabel, 0, 0, 1, 1);
-
-        this.statisticGrid.addRow(1, new Label("Number of animals alive"),
+        this.statisticGrid.addRow(1, new Label("Number of animals alive: "),
                 new Label(Integer.toString(statisticsModule.getAmountOfAnimals())));
-
+        this.statisticGrid.addRow(2, new Label("Number of grasses: "),
+                new Label(Integer.toString(statisticsModule.getAmountOfGrasses())));
+        this.statisticGrid.addRow(3, new Label("Number of free places: "),
+                new Label(Integer.toString(SimulationParameters.mapHeight * SimulationParameters.mapWidth -
+                        statisticsModule.getAmountOfFreePlaces())));
+        this.statisticGrid.addRow(4, new Label("Average energy of alive animals: "),
+                new Label(String.format(Locale.ENGLISH, "%.2f", statisticsModule.getAverageEnergyLifeForAlive())));
+        this.statisticGrid.addRow(5, new Label("Average life span of dead animals: "),
+                new Label(String.format(Locale.ENGLISH, "%.2f", statisticsModule.getAverageAgeForDead())));
     }
 
     public void refresh() {
--- src/main/java/agh/statistics/StatisticsModule.java
+++ src/main/java/agh/statistics/StatisticsModule.java
@@ -5,13 +5,26 @@ import javafx.scene.layout.VBox;
 
 public class StatisticsModule {
     int amountOfAnimals;
-    public int getAmountOfAnimals() {
-        return amountOfAnimals;
-    }
     int amountOfGrasses;
     int amountOfFreePlaces;
     double averageEnergyLifeForAlive;
     double averageAgeForDead;
+
+    public int getAmountOfAnimals() {
+        return amountOfAnimals;
+    }
+    public int getAmountOfGrasses() {
+        return amountOfGrasses;
+    }
+    public int getAmountOfFreePlaces() {
+        return amountOfFreePlaces;
+    }
+    public double getAverageEnergyLifeForAlive() {
+        return averageEnergyLifeForAlive;
+    }
+    public double getAverageAgeForDead() {
+        return averageAgeForDead;
+    }
 
     public void incrementGrasses() {
         amountOfGrasses++;
@@ -20,17 +33,17 @@ public class StatisticsModule {
         amountOfGrasses--;
     }
     public void changeAmountOfFreePlaces(int amount){
-        amountOfFreePlaces=amount;
+        amountOfFreePlaces = amount;
     }
 
     public void changeAmountOfAnimal(int amount) {
-        this.amountOfAnimals=amount;
+        this.amountOfAnimals = amount;
     }
 
     public void changeAverageEnergyLifeForAlive(int sumOfAlive,int amountAlive) {
-    this.averageEnergyLifeForAlive=(double) sumOfAlive/amountAlive;
+        this.averageEnergyLifeForAlive = (double) sumOfAlive/amountAlive;
     }
     public void changeAverageAgeForDead(int sumOfDead,int amountDead) {
-    this.averageAgeForDead=(double) sumOfDead/amountDead;
+        this.averageAgeForDead = (double) sumOfDead/amountDead;
     }
 }
